aba15c6b60ec77a88945a685a90b48177e777f0f
VarunSivamani <varunsivamani@gmail.com>
2022-09-15 19:17:46 +0530

    Sep-15

---
+++ Strings/BackspaceStringCompare.java
@@ -0,0 +1,68 @@
+/*
+
+844. Backspace String Compare
+
+Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+Note that after backspacing an empty text, the text will continue empty.
+
+Example 1:
+Input: s = "ab#c", t = "ad#c"
+Output: true
+
+Explanation: Both s and t become "ac".
+
+Example 2:
+Input: s = "ab##", t = "c#d#"
+Output: true
+
+Explanation: Both s and t become "".
+
+Example 3:
+Input: s = "a#c", t = "b"
+Output: false
+
+Explanation: s becomes "c" while t becomes "b".
+
+*/
+
+// Solution 1
+
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return getString(s).equals(getString(t));
+    }
+
+    public String getString(String s) {
+        StringBuilder str = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '#') {
+                if (str.length() != 0) {
+                    str.deleteCharAt(str.length() - 1);
+                }
+            } else {
+                str.append(ch);
+            }
+        }
+        return str.toString();
+    }
+}
+
+// Solution 2
+
+class Solution2 {
+    public boolean backspaceCompare(String S, String T) {
+        return build(S).equals(build(T));
+    }
+
+    public String build(String S) {
+        Stack<Character> ans = new Stack();
+        for (char c : S.toCharArray()) {
+            if (c != '#')
+                ans.push(c);
+            else if (!ans.empty())
+                ans.pop();
+        }
+        return String.valueOf(ans);
+    }
+}
