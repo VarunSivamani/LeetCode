91ad33c458d2ff376dbd2c4a30c85369f0ce6f59
VarunSivamani <varunsivamani@gmail.com>
2023-03-05 12:21:41 +0530

    Mar-05

---
+++ Arrays/MaximumIceCreamBars.java
@@ -0,0 +1,74 @@
+/*
+
+1833. Maximum Ice Cream Bars
+
+It is a sweltering summer day, and a boy wants to buy some ice cream bars.
+At the store, there are n ice cream bars. You are given an array costs of length n, where costs[i] is the price of the ith ice cream bar in coins. The boy initially has coins coins to spend, and he wants to buy as many ice cream bars as possible. 
+Note: The boy can buy the ice cream bars in any order.
+
+Return the maximum number of ice cream bars the boy can buy with coins coins.
+You must solve the problem by counting sort.
+
+Example 1:
+Input: costs = [1,3,2,4,1], coins = 7
+Output: 4
+
+Explanation: The boy can buy ice cream bars at indices 0,1,2,4 for a total price of 1 + 3 + 2 + 1 = 7.
+
+Example 2:
+Input: costs = [10,6,8,7,7,8], coins = 5
+Output: 0
+
+Explanation: The boy cannot afford any of the ice cream bars.
+
+Example 3:
+Input: costs = [1,6,3,1,2,5], coins = 20
+Output: 6
+
+Explanation: The boy can buy all the ice cream bars for a total price of 1 + 6 + 3 + 1 + 2 + 5 = 18.
+
+*/
+
+// Solution - 1
+
+class Solution {
+
+  public int maxIceCream(int[] costs, int coins) {
+    Arrays.sort(costs);
+    int n = costs.length;
+
+    for (int i = 0; i < n; i++) {
+      if (coins >= costs[i]) {
+        coins -= costs[i];
+      } else {
+        return i;
+      }
+    }
+    return n;
+  }
+}
+
+// Solution - 2
+
+class Solution2 {
+
+  public int maxIceCream(int[] costs, int coins) {
+    Arrays.sort(costs);
+    int n = costs.length;
+    int dp[] = new int[n];
+    dp[0] = costs[0];
+
+    if (dp[0] > coins) {
+      return 0;
+    }
+
+    for (int i = 1; i < n; i++) {
+      dp[i] = costs[i] + dp[i - 1];
+
+      if (dp[i] > coins) {
+        return i;
+      }
+    }
+    return n;
+  }
+}
